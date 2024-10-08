2a28a8162d2677aec0f3b199c6e83655e1b8f293
ChuMutt <chufilthymutt@gmail.com>
2024-10-08 11:37:40 -0500

    refactor

--- A-000-name-generator/cpp/v1/main.cpp
+++ A-000-name-generator/cpp/v1/main.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 using namespace std;
 
+string firstNameList[4] = {"Ben", "Michelle", "Frederick", "Deltrese"};
+string lastNameList[4] = {"Doe", "Shelly", "Fitzgeraldine", "Shoemaker"};
+
 int main() {
-    srand(time(0)); // Seed a new sequence of pseudo-random numbers.
+  srand(time(0)); // Seed a new sequence of pseudo-random numbers.
 
-    string firstNameList[4] =
-        {"Ben", "Michelle", "Frederick", "Deltrese"};
-    string lastNameList[4] =
-        {"Doe", "Shelly", "Fitzgeraldine", "Shoemaker"};
+  int RandomIndex = rand() % 4;
 
-    int RandomIndex = rand() % 4;
+  string firstName = firstNameList[RandomIndex];
+  string lastName = lastNameList[RandomIndex];
 
-    string firstName = firstNameList[RandomIndex];
-    string lastName = lastNameList[RandomIndex];
+  cout << firstName << " " << lastName << " \n";
 
-    cout << firstName << " " << lastName << " \n";
-
-    return 0;
+  return 0;
 }
